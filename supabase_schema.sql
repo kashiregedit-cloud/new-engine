3c818de0dd9fb8f0183a362064163ca749dc77e9
kashiregedit-cloud <kashiregedit@gmail.com>
2026-01-21 12:27:31 +0600

    Add missing status column to whatsapp_sessions fix in schema

--- supabase_schema.sql
+++ supabase_schema.sql
@@ -60,4 +60,5 @@ create table if not exists session_qr_link (
 -- 6. Fix for existing whatsapp_sessions table
 alter table whatsapp_sessions add column if not exists user_id text;
 alter table whatsapp_sessions add column if not exists qr_code text;
+alter table whatsapp_sessions add column if not exists status text default 'stopped';
 
